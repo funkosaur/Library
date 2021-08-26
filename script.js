0c798926fb43cf78cf86f59593e4f97d85c24792
funkosaur <mario.bojkovski@outlook.com>
2021-08-27 00:16:55 +0100

    finished form

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const addNewBook = document.querySelector("#addBook");
 const form = document.querySelector(".form")
 
-addNewBook.addEventListener(click, ()=>{
-    form.style.display = "none";
+addNewBook.addEventListener("click", ()=>{
+    if(form.style.height == "0vh"){
+        form.style.height = "60vh";
+        form.style.padding = "1rem 0 1rem 0";
+        form.style.boxShadow =  "0px 0px 4px 4px rgb(158, 158, 158)";
+    } else {
+    form.style.height = "0vh";
+    form.style.padding = "0";
+    form.style.boxShadow =  "none";
+}
+    
 
-})
+});
