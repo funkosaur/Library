6cddfda773c2b8a6ea773b9d59bdf82a03cd4bb1
funkosaur <mario.bojkovski@outlook.com>
2021-08-27 16:23:55 +0100

    functionalization

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const addNewBook = document.querySelector("#addBook");
-const form = document.querySelector(".form")
+const form = document.querySelector(".form");
+const submit = document.querySelector("#submitted");
 
 addNewBook.addEventListener("click", ()=>{
     if(form.style.height == "0vh"){
@@ -11,6 +12,39 @@ addNewBook.addEventListener("click", ()=>{
     form.style.padding = "0";
     form.style.boxShadow =  "none";
 }
+});
+
+let myLibrary = [];
+
+submit.addEventListener("click", () => {
+    const authorName = document.querySelector("#authorName");
+    const bookName = document.querySelector("#bookName");
+    const pageNumber = document.querySelector("#pageNumber");
+    const readStatus = document.querySelector("#readStatus");
+    const bookPicture = document.querySelector("#pictureURL");
     
+    let newestBook = {
+        author : authorName.value,
+        book : bookName.value,
+        pages : pageNumber.value,
+        read : readStatus.value,
+        picture : bookPicture.value
+
+    }
+
+    myLibrary.push(newestBook);
+    console.log(myLibrary)
+
+    myLibrary.forEach(book => {
+        const bookDivs = document.createElement("div");
+        bookDivs.classList.toggle("books");
+        
+    })
+    
+    authorName.value = "";
+    bookName.value = "";
+    pageNumber.value = "";
+    readStatus.value = "";
+    bookPicture.value = "";
 
 });
