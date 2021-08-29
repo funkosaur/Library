fc5ef7f94700617c74dbea99baaf46aa499f8283
funkosaur <mario.bojkovski@outlook.com>
2021-08-29 01:30:25 +0100

    responsiveness

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const addNewBook = document.querySelector("#addBook");
 const form = document.querySelector(".form");
 const submit = document.querySelector("#submitted");
+const layout = document.querySelector(".layout");
+
 
 addNewBook.addEventListener("click", ()=>{
+    const width  = window.innerWidth || document.documentElement.clientWidth || 
+document.body.clientWidth;
+    
     if(form.style.height == "0vh"){
-        form.style.height = "60vh";
+       
+        if(width <= 370){
+             form.style.height = "100vh";
+            
+        } else if(width <= 608){
+             form.style.height = "80vh";
+            
+        } else if(width <= 1007){
+             form.style.height = "70vh";
+            
+        } else { form.style.height = "60vh"; }
+        
         form.style.padding = "1rem 0 1rem 0";
         form.style.boxShadow =  "0px 0px 4px 4px rgb(158, 158, 158)";
+        layout.style.gridTemplateRows = "0.2fr 0.2fr 1fr 1fr 0.2fr"
     } else {
     form.style.height = "0vh";
     form.style.padding = "0";
     form.style.boxShadow =  "none";
+    layout.style.gridTemplateRows = "0.2fr 0.2fr 0.1fr 1fr 0.2fr"
 }
 });
 
