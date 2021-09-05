dcd30fbd1c5a66dba7ffda9cf772ac6458343bc0
funkosaur <mario.bojkovski@outlook.com>
2021-09-05 01:25:37 +0100

    test

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const bookSies = document.querySelectorAll(".books");
 const delButton = document.querySelectorAll("#del-button");
 
   
+setTimeout(function () {
+    let viewheight = $(window).height();
+    let viewwidth = $(window).width();
+    let viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
+}, 300);
+
+
 
 window.addEventListener("resize", ()=>{
 
