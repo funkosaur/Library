d809ae3ff0d4d117360799b8fd8381fd87ed62b2
funkosaur <mario.bojkovski@outlook.com>
2021-09-05 14:15:40 +0100

    test

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ setTimeout(function () {
     let viewheight = window.visualViewport.height;
     let viewwidth = window.visualViewport.width;
     let viewport = document.querySelector("meta[name=viewport]");
-    viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
+    viewport.setAttribute("content", "height=" + viewheight + ", width=" + viewwidth + ", initial-scale=1.0");
 }, 300);
 
 
