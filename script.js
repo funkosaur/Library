b563d342c5244632b5943cb1db9e9152afdce6c0
funkosaur <mario.bojkovski@outlook.com>
2021-09-05 23:21:02 +0100

    testing

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ setTimeout(function () {
 }, 300);
 
 
-
+/*
 window.addEventListener("resize", ()=>{
 
     const width  = window.innerWidth || document.documentElement.clientWidth || 
@@ -32,6 +32,8 @@ document.body.clientWidth;
        
    } else { form.style.height = "60vh"; }
 })
+*/
+let originalHeight = form.offsetHeight;
 
 
 addNewBook.addEventListener("click", ()=>{
@@ -44,26 +46,18 @@ addNewBook.addEventListener("click", ()=>{
 document.body.clientWidth;
     
     if(form.style.height == "0vh"){
+        
        
-        if(width <= 370){
-             form.style.height = "90vh";
-            
-        } else if(width <= 608){
-             form.style.height = "80vh";
-            
-        } else if(width <= 1007){
-             form.style.height = "70vh";
-            
-        } else { form.style.height = "60vh"; }
+        form.style.height = originalHeight + "px"; 
         form.style.overflow = "auto"
-        form.style.padding = "1rem 0 1rem 0";
+        form.style.padding = "1rem 0rem 1rem 0rem";
         form.style.boxShadow =  "0px 0px 4px 4px rgb(158, 158, 158)";
         library.style.marginTop = "2rem";
         
     } else {
     form.style.overflow = "hidden"
     form.style.height = "0vh";
-    form.style.padding = "0";
+    form.style.padding = "0rem";
     form.style.boxShadow =  "none";
     library.style.marginTop = "0rem";
     
