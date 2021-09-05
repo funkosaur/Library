f18e94942be80160b11d15965719a367999a846d
funkosaur <mario.bojkovski@outlook.com>
2021-09-05 14:06:44 +0100

    test

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const delButton = document.querySelectorAll("#del-button");
 
   
 setTimeout(function () {
-    let viewheight = $(window).height();
-    let viewwidth = $(window).width();
+    let viewheight = window.visualViewport.height;
+    let viewwidth = window.visualViewport.width;
     let viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
 }, 300);
